bb6cb6669adc6ec80e718a89bf36e2519d2b9965
edb-c <edb.cofresi@gmail.com>
2018-08-05 00:57:19 -0400

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
+
+require_relative '../lib/marvel_characters/scraper'
+require_relative '../lib/marvel_characters/restaurant'
+require_relative '../lib/marvel_characters/cli'
+
