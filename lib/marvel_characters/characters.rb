3c9964c0f4f43de50d0512d6df8600c21565124f
Ethel C <cofresi.ethel@gmail.com>
2018-08-21 19:09:29 +0000

    Changed Characters File

--- lib/marvel_characters/characters.rb
+++ lib/marvel_characters/characters.rb
@@ -1,42 +1,48 @@
-class MarvelCharacters::Character
+class MarvelCharacters::Characters
 
   attr_accessor :super_name, :real_name, :character_type, :number_of_powers
-
+=begin
   @@all = []
-
   def initialize(super_name=nil, real_name=nil, character_type=nil, number_of_powers=nil)
-
     @super_name = super_name
     @real_name = real_name
     @character_type = character_type
     @number_of_powers = number_of_powers
-    @@aoll << self
-
+    @@all << self
   end
 
   def self.all
-    @@all
+    puts @@all
   end
 
   def self.find(number)
-    self.all(number-1)
+    self.all(number)
+  end
+=end
+  def self.get_character_info(input)
+   MarvelCharacters::Scraper.new.get_character_page(input)
   end
 
-  def get_super_name
-    super_name = parsed_character_page.css('div.wiki-details').css('table').css('tr')[0].css('td').css('div').css('span').text
+  def self.get_super_name(input)
+
+    super_name = self.get_character_info(input).css('div.wiki-details').css('table').css('tr')[0].css('td').css('div').css('span').text
+    #puts @super_name
   end
 
-  def get_real_name
-    real_name  = parsed_character_page.css('div.wiki-details').css('table').css('tr')[1].css('td').css('div').css('span').text
+  def self.get_real_name(input)
+    real_name  = self.get_character_info(input).css('div.wiki-details').css('table').css('tr')[1].css('td').css('div').css('span').text
   end
 
-  def get_character_type
-    character_type = parsed_character_page.css('div.wiki-details').css('table').css('tr')[6].css('td').css('div').css('span').text
+  def self.get_character_type(input)
+    character_type = self.get_character_info(input).css('div.wiki-details').css('table').css('tr')[6].css('td').css('div').css('span').text
   end
 
-  def get_number_of_powers
-    number_of_powers = parsed_character_page.css('div.wiki-details').css('table').css('tr')[11].css('td/div[1]/div').length
+  def self.get_number_of_powers(input)
+    number_of_powers = self.get_character_info(input).css('div.wiki-details').css('table').css('tr')[11].css('td/div[1]/div').length
+  end
+
+  def self.get_list_of_powers(input)
+    list_of_powers =  self.get_character_info(input).css('div.wiki-details').css('table').css('tr')[11].css('td/div[1]/div').children.each{|one_power| puts one_power}
   end
 
 end #end Class
-
--- lib/marvel_characters/scraper.rb
+++ lib/marvel_characters/scraper.rb
@@ -3,16 +3,19 @@ require 'nokogiri'
 
 class MarvelCharacters::Scraper
 
-  def get_parsed_pages
+  def get_main_page
 
     main_url= "https://comicvine.gamespot.com/profile/theoptimist/lists/top-100-marvel-characters/32199/"
-
     document = open(main_url)
     content = document.read
     parsed_main_page= Nokogiri::HTML(content)
 
+  end
+
+  def get_character_page(input)
+
     base_character_url = "https://comicvine.gamespot.com"
-    character_url = parsed_main_page.css('div#default-content a')[10]["href"]
+    character_url = self.get_main_page.css('div#default-content a')[input]["href"]
     final_character_url = "#{base_character_url}#{character_url}"
     document2 = open(final_character_url)
     content2 = document2.read
