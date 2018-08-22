ef92a7838dad2da4234154df7e0796e805f9c5df
edb-c <edb.cofresi@gmail.com>
2018-08-21 23:32:33 -0400

    Update characters.rb

--- lib/marvel_characters/characters.rb
+++ lib/marvel_characters/characters.rb
@@ -1,6 +1,8 @@
 class MarvelCharacters::Characters
 
   attr_accessor :super_name, :real_name, :character_type, :number_of_powers
+  
+  
 =begin
   @@all = []
   def initialize(super_name=nil, real_name=nil, character_type=nil, number_of_powers=nil)
@@ -19,22 +21,49 @@ class MarvelCharacters::Characters
     self.all(number)
   end
 =end
+=begin
+ puts "The super name is " + character_array[0].to_s
+ puts "The real name is " + character_array[1].to_s
+ puts "The character type is " + character_array[2].to_s
+ puts "The number of powers is " + character_array[3].to_s
+ puts "The list of powers are : " + character_array[4].to_s
+=end
 
-  def self.get_character_info(input)
+def self.get_character_info(input)
+
    character_page = MarvelCharacters::Scraper.new.get_character_page(input)
-   character_page.css('div.wiki-details').css('table').css('tr')[0].css('td').css('div').css('span').text
+   
+   character_array = []
+   
+   self.super_name = character_page.css('div.wiki-details').css('table').css('tr')[0].css('td').css('div').css('span').text
+   
+   real_name  = character_page.css('div.wiki-details').css('table').css('tr')[1].css('td').css('div').css('span').text
+  
+   character_type = character_page.css('div.wiki-details').css('table').css('tr')[6].css('td').css('div').css('span').text
+  
+   number_of_powers = character_page.css('div.wiki-details').css('table').css('tr')[11].css('td/div[1]/div').length
 
+   list_of_powers =  character_page.css('div.wiki-details').css('table').css('tr')[11].css('td/div[1]/div').children.each{|one_power| puts one_power}
+  
+   character_array << super_name << real_name << character_type << number_of_powers << list_of_powers
 
-  end
+#   character_array
+end
 
   def self.get_super_name(input)
+
+    super_name
+
+#    self.get_character_info(input)[0].to_s
 
 #    @@super_name = self.get_character_info(input).css('div.wiki-details').css('table').css('tr')[0].css('td').css('div').css('span').text
 
   end
 
   def self.get_real_name(input)
-    real_name  = self.get_character_info(input).css('div.wiki-details').css('table').css('tr')[1].css('td').css('div').css('span').text
+
+    self.get_character_info(input)[1].to_s
+#    real_name  = self.get_character_info(input).css('div.wiki-details').css('table').css('tr')[1].css('td').css('div').css('span').text
   end
 
   def self.get_character_type(input)
