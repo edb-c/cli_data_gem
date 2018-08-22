02fe5b31d49dd859df8622d631be8b8afe298ade
edb-c <edb.cofresi@gmail.com>
2018-08-22 01:41:27 -0400

    Update cli.rb

--- lib/marvel_characters/cli.rb
+++ lib/marvel_characters/cli.rb
@@ -30,16 +30,27 @@ class MarvelCharacters::CLI
   end #end start
 
   def print_character(input)
-    character = MarvelCharacters::Characters
-    # puts "Super Name: " + character.get_character_info(input).to_s
+    character_array = MarvelCharacters::Characters.get_character_info(input)
+    
+    puts "Super Name:       "      +   character_array[0].to_s
+    puts "Real Name:        "      +   character_array[1].to_s
+    puts "Character Type:   "      +   character_array[2].to_s
+    puts "Number of Powers: "      +   character_array[3].to_s
+    #puts "List of Powers:   "     +    list_of_powers
+    
+    #character_array[4][.each {|one_power| puts one_power}
+    #  puts character_array[4][0].to_s
+    
+    
+    #puts "Super Name: " + character.get_character_info(input).to_s
     #    puts "Super Name: #{character.get_character_info(input)}"
-     puts "Super Name:          #{character.get_super_name(input)}"
+    #puts "Super Name:          #{character.get_super_name}"
      #    puts "Super Name:          #{character.super_name}"
      #    puts "Real Name:           #{character.get_real_name(input)}"
 #    puts "Character Type:      #{character.get_character_type(input)}"
 #    puts "Number of Powers:    #{character.get_number_of_powers(input)}"
-#    puts "List of powers are:"
-#    puts character.get_list_of_powers(input)
+    puts "List of powers are:"
+    puts MarvelCharacters::Characters.get_list_of_powers(input)
     puts ""
     puts '--------------------------------'
   end
